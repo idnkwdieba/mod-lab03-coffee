20fade2f5b951e01b80ea55866a25ac9dfecbe97
Виктор Финаев <146958946+idnkwdieba@users.noreply.github.com>
2024-04-13 19:48:32 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,3 +1,4 @@
+// Copyright 2022 UNN-IASR
 #ifndef INCLUDE_AUTOMATA_H_
 #define INCLUDE_AUTOMATA_H_
 #include <string>
@@ -6,24 +7,24 @@
 enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
 
 class Automata {
-private:
-	int cash;
-	std::vector<std::string> menu;
-	std::vector<int> prices;
-	STATES state;
-public:
-	Automata();
-	void on();
-	void off();
-	void coin(int coins);
-	std::vector<std::string> getMenu();
-	STATES getState();
-	void choice(int choice);
-	void cancel();
-private:
-	void check(int choice);
-	void cook(int choice);
-	void finish();
+ private:
+  int cash;
+  std::vector<std::string> menu;
+  std::vector<int> prices;
+  STATES state;
+ public:
+  Automata();
+  void on();
+  void off();
+  void coin(int coins);
+  std::vector<std::string> getMenu();
+  STATES getState();
+  void choice(int choice);
+  void cancel();
+ private:
+  void check(int choice);
+  void cook(int choice);
+  void finish();
 };
 
 #endif // INCLUDE_AUTOMATA_H_
